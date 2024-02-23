3626044abe6e7ea3366c35a8fda75065a138b079
luckychacha <358140284@qq.com>
2024-02-23 10:20:23 +0800

    Use hex macro.

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@ use hex_literal::hex;
 fn main() {
     let hash = Sha256::digest(b"Hello, world!");
     println!("{:x}", hash);
-
+    // let a  = "Hello, world!".as_bytes();
+    // hex! 宏主要用于将十六进制字符串转换为字节数组
+    println!("{:?}", b"Hello, world!");
+    
     let mut hasher = Sha256::new();
     hasher.update(b"hello world");
-    let _hash256 = hasher.finalize();
+    let hash256: [u8; 32] = hasher.finalize().try_into().unwrap();
 
-
-    let mut hasher = Sha512::new();
-    hasher.update(b"hello world");
-    let _hash512 = hasher.finalize();
+    assert_eq!(hash256, hex!("b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9"));
 
 }
