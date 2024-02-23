571fc015de7004cd6a84ef7ccaf6d58840086c64
luckychacha <358140284@qq.com>
2024-02-23 10:21:40 +0800

    Make error readble.

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     
     let mut hasher = Sha256::new();
     hasher.update(b"hello world");
-    let hash256: [u8; 32] = hasher.finalize().try_into().unwrap();
+    let hash256: [u8; 32] = hasher.finalize().try_into().expect("Wrong length");
 
     assert_eq!(hash256, hex!("b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9"));
 
