e26d4222fe85f22d89a9fea334f909d7351c27b1
luckychacha <358140284@qq.com>
2024-02-29 08:17:13 +0800

    Feat: Get checksum.

--- bitcoin/src/mnemonic.rs
+++ bitcoin/src/mnemonic.rs
@@ -1,6 +1,6 @@
 use hmac::Hmac;
 use pbkdf2::pbkdf2;
-use sha2::Sha512;
+use sha2::{Digest, Sha256, Sha512};
 
 // Mnemonic
 pub struct Mnemonic<'a> {
@@ -31,6 +31,28 @@ impl Mnemonic<'_> {
 	}
 }
 
+pub fn get_checksum(input: &str) -> String {
+	// Convert input to bytes
+	let input = hex::decode(input).unwrap();
+	
+	let mut hasher = Sha256::new();
+	hasher.update(input);
+	let result = hasher.finalize();
+	// 将结果转换为十六进制字符串
+    let hashed_hex = hex::encode(result);
+	println!("Hashed hex: {}", hashed_hex);
+	// 获取哈希值的第一个字节
+    let first_byte = result[0];
+
+    // 将第一个字节的前4位转换为二进制字符串
+    let first_4_bits = format!("{:b}", first_byte >> 4);
+
+    // 打印前4位二进制数
+    println!("First 4 bits: {}", first_4_bits);
+	
+	first_4_bits
+}
+
 #[cfg(test)]
 mod tests {
 	use super::*;
@@ -45,5 +67,12 @@ mod tests {
 
 		let seed = mnemonic_seed.to_seed();
 		assert_eq!(hex::encode(seed), "2e8905819b8723fe2c1d161860e5ee1830318dbf49a83bd451cfb8440c28bd6fa457fe1296106559a3c80937a1c1069be3a3a5bd381ee6260e8d9739fce1f607")
+	}
+
+	#[test]
+	fn test_get_checksum() {
+		let input = "16432a207785ec5c4e5a226e3bde819d";
+		let left = get_checksum(input);
+		assert_eq!(left, "0111");
 	}
 }
