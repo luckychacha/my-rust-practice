b394fef5c9ad3d88438ad3f06b09df1a2a220178
luckychacha <358140284@qq.com>
2024-03-28 18:24:18 +0800

    Add concurrency examples.

--- common/examples/mpsc-example.rs
+++ common/examples/mpsc-example.rs
@@ -2,10 +2,16 @@ use std::{sync::mpsc, thread, time::Duration};
 
 fn main() {
     let (s1, r1) = mpsc::channel();
+    let s1_clone = s1.clone();
+
+    // Just for init the session! LOL
+    thread::spawn(move || {
+        s1_clone.send("Hello World!").unwrap();
+    });
+
     let (s2, r2) = mpsc::channel();
 
     thread::spawn(move || {
-        s1.send("Hello World!").unwrap();
         for message in r2 {
             println!("Thread1 Received: {}", message);
             s1.send("Hello World!").unwrap();
